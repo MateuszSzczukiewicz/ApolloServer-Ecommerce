df91238b1618e602e6af538d4f7b2d09c982549d
MateuszSzczukiewicz <szcmat6277@gmail.com>
2024-08-15 18:25:10 +0200

    Added product model to prisma

--- src/schema/resolvers/Query/product.ts
+++ src/schema/resolvers/Query/product.ts
@@ -1,5 +1,5 @@
-import { prisma } from "../../../../db.js";
-import type { QueryResolvers } from "./../../../types.generated";
+import { QueryResolvers } from "@/graphql/types.generated";
+import { prisma } from "@/db";
 
 export const product: NonNullable<QueryResolvers["product"]> = async (_parent, arg, _ctx) => {
 	const product = await prisma.product.findUnique({
--- src/schema/resolvers/Query/products.ts
+++ src/schema/resolvers/Query/products.ts
@@ -1,4 +1,4 @@
-import type { QueryResolvers } from "./../../../graphql/types.generated";
+import type { QueryResolvers } from "@/graphql/types.generated";
 export const products: NonNullable<QueryResolvers["products"]> = async (_parent, _arg, _ctx) => {
 	/* Implement Query.products resolver logic here */
 };
