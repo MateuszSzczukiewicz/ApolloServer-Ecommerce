c227f2bcd85df88057a716c3193f79150b42efa5
MateuszSzczukiewicz <szcmat6277@gmail.com>
2024-09-02 10:55:40 +0200

    Added vercel configuration

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,10 @@ const server = new ApolloServer({
 	resolvers,
 });
 
+const port = Number(process.env.PORT) || 4000;
+
 const { url } = await startStandaloneServer(server, {
-	listen: { port: 4000 },
+	listen: { port },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
